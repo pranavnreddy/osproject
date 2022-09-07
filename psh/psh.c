490b5699e7a8f22c1116a1f2b07fa0442e56c10d
Pranav Reddy <pranav.n.reddy@gmail.com>
2022-09-07 13:11:50 -0700

    contributing more to psh

--- psh/psh.c
+++ psh/psh.c
@@ -1,67 +1,89 @@
-include <stdlib.h>
+#include <stdlib.h>
 
 #define PSH_RL_BUFSIZE 1024
+#define PSH_TOK_BUFSIZE 64
 #define PSH_TOK_DELIM " \t\r\n\a"
 
-int main(int argc, int argv**) {
-	psh_loop();
-	return EXIT_SUCCESS;
+int main(int argc, char **argv) {
+    psh_loop();
+    return EXIT_SUCCESS;
 }
 
 void lsh_loop() {
-	char *line;
-	char **args;
-	int status;
+    char *line;
+    char **args;
+    int status;
 
-	do {
-		printf("> ");
-		line = lsh_read_line();
-		args = lsh_split_line(line);
-		status = lsh_execute(args);
+    do {
+        printf("> ");
+        line = lsh_read_line();
+        args = lsh_split_line(line);
+        status = lsh_execute(args);
 
-		free(line);
-		free(args);
-	} while (status);
+        free(line);
+        free(args);
+    } while (status);
 }
 
 char *lsh_read_line(void) {
-	int bufsize = PSH_RL_BUFSIZE;
-	int position = 0;
-	char *buffer = malloc(sizeof(char) * bufsize);
-	int c;
+    int bufsize = PSH_RL_BUFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * bufsize);
+    int c;
 
-	if(!buffer) {
-		fprintf(stderr, "psh: allocation error\n");
-		exit(EXIT_FAILURE);
-	}
+    if (!buffer) {
+        fprintf(stderr, "psh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
 
-	while(1) {
-		c = getchar();
+    while (1) {
+        c = getchar();
 
-		if(c == EOF || c == '\n') {
-			buffer[position] = '\0';
-			return buffer;
-		} else {
-			buffer[position] = c;
-		}
-		position++;
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+        position++;
 
-		if(position >= bufsize) {
-			bufsize += PSH_RL_BUFSIZE;
-			buffer = realloc(buffer, bufsize);
-			if(!buffer) {
-				fprintf(stderr, "psh: allocation error\n");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
+        if (position >= bufsize) {
+            bufsize += PSH_RL_BUFSIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer) {
+                fprintf(stderr, "psh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
 }
 
 char **lsh_split_line(char *line) {
-	int bufsize = PSH_TOK_BUFSIZE, position = 0;
-	char **tokens = malloc(bufsize * sizeof(char*));
-	char *token;
+    int bufsize = PSH_TOK_BUFSIZE, position = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
 
-	
-	return tokens;
+    if (!tokens) {
+        fprintf(stderr, "psh: allocation error");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, PSH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= bufsize) {
+            bufsize += PSH_TOK_BUFSIZE;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens) {
+                fprintf(stderr, "psh: allocation error");
+                exit(EXIT_FAILURE);
+            }
+        }
+		token = strtok(NULL, PSH_TOK_DELIM);
+    }
+
+	tokens[position] = NULL;
+    return tokens;
 }
