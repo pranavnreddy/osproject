185a63f7d51c0ce106cdd0e520d2503352a950ec
Pranav Reddy <pranav.n.reddy@gmail.com>
2022-09-07 10:58:50 -0700

    started shell psh

---
+++ psh/psh.c
@@ -0,0 +1,67 @@
+include <stdlib.h>
+
+#define PSH_RL_BUFSIZE 1024
+#define PSH_TOK_DELIM " \t\r\n\a"
+
+int main(int argc, int argv**) {
+	psh_loop();
+	return EXIT_SUCCESS;
+}
+
+void lsh_loop() {
+	char *line;
+	char **args;
+	int status;
+
+	do {
+		printf("> ");
+		line = lsh_read_line();
+		args = lsh_split_line(line);
+		status = lsh_execute(args);
+
+		free(line);
+		free(args);
+	} while (status);
+}
+
+char *lsh_read_line(void) {
+	int bufsize = PSH_RL_BUFSIZE;
+	int position = 0;
+	char *buffer = malloc(sizeof(char) * bufsize);
+	int c;
+
+	if(!buffer) {
+		fprintf(stderr, "psh: allocation error\n");
+		exit(EXIT_FAILURE);
+	}
+
+	while(1) {
+		c = getchar();
+
+		if(c == EOF || c == '\n') {
+			buffer[position] = '\0';
+			return buffer;
+		} else {
+			buffer[position] = c;
+		}
+		position++;
+
+		if(position >= bufsize) {
+			bufsize += PSH_RL_BUFSIZE;
+			buffer = realloc(buffer, bufsize);
+			if(!buffer) {
+				fprintf(stderr, "psh: allocation error\n");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
+
+char **lsh_split_line(char *line) {
+	int bufsize = PSH_TOK_BUFSIZE, position = 0;
+	char **tokens = malloc(bufsize * sizeof(char*));
+	char *token;
+
+	
+	return tokens;
+}
