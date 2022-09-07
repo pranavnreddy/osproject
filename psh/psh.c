9cbc4449dbacf730acc64b7155b9a6759362eb4a
Pranav Reddy <pranav.n.reddy@gmail.com>
2022-09-07 13:46:46 -0700

    added cd, help, and exit as builtins

--- psh/psh.c
+++ psh/psh.c
@@ -4,28 +4,34 @@
 #define PSH_TOK_BUFSIZE 64
 #define PSH_TOK_DELIM " \t\r\n\a"
 
+int psh_cd(char **args);
+int psh_help(char **args);
+int psh_exit(char **args);
+
+char *builtin_str[] = {"cd", "help", "exit"};
+
 int main(int argc, char **argv) {
     psh_loop();
     return EXIT_SUCCESS;
 }
 
-void lsh_loop() {
+void psh_loop() {
     char *line;
     char **args;
     int status;
 
     do {
         printf("> ");
-        line = lsh_read_line();
-        args = lsh_split_line(line);
-        status = lsh_execute(args);
+        line = psh_read_line();
+        args = psh_split_line(line);
+        status = psh_execute(args);
 
         free(line);
         free(args);
     } while (status);
 }
 
-char *lsh_read_line(void) {
+char *psh_read_line(void) {
     int bufsize = PSH_RL_BUFSIZE;
     int position = 0;
     char *buffer = malloc(sizeof(char) * bufsize);
@@ -58,7 +64,7 @@ char *lsh_read_line(void) {
     }
 }
 
-char **lsh_split_line(char *line) {
+char **psh_split_line(char *line) {
     int bufsize = PSH_TOK_BUFSIZE, position = 0;
     char **tokens = malloc(bufsize * sizeof(char *));
     char *token;
@@ -81,9 +87,68 @@ char **lsh_split_line(char *line) {
                 exit(EXIT_FAILURE);
             }
         }
-		token = strtok(NULL, PSH_TOK_DELIM);
+        token = strtok(NULL, PSH_TOK_DELIM);
     }
 
-	tokens[position] = NULL;
+    tokens[position] = NULL;
     return tokens;
+}
+
+int psh_launch(char **args) {
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+
+    if (pid == 0) {
+        if (execvp(args[0], args) == -1) {
+            perror("psh");
+        }
+        exit(EXIT_FAILURE);
+    } else if (pid < 0) {
+        perror("psh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+    return 1;
+}
+
+int (*builtin_func[])(char **) = {
+    &psh_cd,
+    &psh_help,
+    &psh_exit,
+};
+
+int psh_num_builtins() {
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+int psh_cd(char **args) {
+    if (args[1] == NULL) {
+        fprintf(stderr, "psh: expected argument to \"cd\"\n");
+    } else {
+        if (chdir(args[1] != 0)) {
+            perror("psh");
+        }
+    }
+    return 1;
+}
+
+int psh_help(char **args) {
+    int i;
+    printf("This is Pranav Reddy's shell.\n");
+    printf("It is based on Stephen Brennan's lsh tutorial\n");
+    printf("You can find a link to it here: https://brennan.io/2015/01/16/write-a-shell-in-c/\n\n");
+    printf("The following commands are built-in:\n");
+    for (i = 0; i < psh_num_builtins(); i++) {
+        printf("	%s\n", builtin_str[i]);
+    }
+    printf("Use the man command for information on other programs.\n");
+    return 1;
+}
+
+int psh_exit(char **args) {
+    return 0;
 }
